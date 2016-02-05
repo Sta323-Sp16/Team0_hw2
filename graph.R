ce0f9cb872074d1c76aa5762194e0170df8d6381
Colin Rundel <rundel@gmail.com>
2016-02-05 19:16:11 +0000

    Added adj_matrix function

--- graph.R
+++ graph.R
@@ -26,3 +26,42 @@ shortest_path = function(g, v1, v2)
 {
     return(character())
 }
+
+
+
+g = list(A = list(edges   = c(2L),
+                  weights = c(1 )),
+         B = list(edges   = c(3L),
+                  weights = c(1 )),
+         C = list(edges   = c(5L),
+                  weights = c(1 )),
+         D = list(edges   = c(2L),
+                  weights = c(1 )),
+         E = list(edges   = c(4L,6L),
+                  weights = c(1,1  )),
+         F = list(edges   = c(),
+                  weights = c())
+)
+
+
+adj_matrix = function(g)
+{
+  stopifnot(is_valid(g))
+  
+  m = matrix(0, nrow=length(g), ncol=length(g))
+  rownames(m) = names(g)
+  colnames(m) = names(g)
+  
+  for(j in seq_along(g))
+  {
+    node = g[[j]]
+    
+    for(i in seq_along(node$edges))
+    {
+      m[j, node$edges[i]] = node$weights[i]
+    }
+  }
+  
+  return(m)
+}
+
